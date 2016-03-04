7ceb1f71ddff1fb093c67ab51667ee2822338cec
Skrupellos <skruppy@onmars.eu>
2016-03-04 23:44:50 +0100

    Reformatted network stuff

--- network.hs
+++ network.hs
@@ -1,12 +1,38 @@
 import Network.Socket as NS
-import qualified Data.ByteString.Char8 as BS8
 import Control.Exception
 import Control.Monad.Error
 -- import Sm
 import System.IO
 import System.Console.ANSI
 
-hints = NS.defaultHints { NS.addrFlags = [NS.AI_ADDRCONFIG], NS.addrSocketType = NS.Stream }
+
+
+netKnockKnock [] = do
+    putStrLn "Noooose, I can't connect to the server. The washing machine ate all my socks"
+    error "No valid address to connect to"
+
+netKnockKnock (x:xs) = do
+    putStr $ "Connecting to "++(show $ addrAddress x)
+    sock <- socket (addrFamily x) Stream defaultProtocol
+    res  <- try $ connect sock (addrAddress x) :: IO (Either SomeException ())
+    case res of
+        Left _ -> do
+            putStrLn " ✘"
+            sClose sock
+            netKnockKnock xs
+        Right _ -> do
+            putStrLn " ✔"
+            return sock
+
+
+netConnect host port = withSocketsDo $ do
+    addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+    sock     <- netKnockKnock addrInfo
+    return sock
+    where
+        hints = NS.defaultHints {
+            NS.addrFlags      = [NS.AI_ADDRCONFIG],
+            NS.addrSocketType = NS.Stream }
 
 
 netListen hdl = do
@@ -38,30 +64,18 @@ netConverse hdl (line:remaining) = do
 
 
 main = do
-    hdl <- client "localhost" 1357 -- get a connected socket
+    -- Get socket
+    socket <- netConnect "sysprak.onmars.eu" "1357"
+    
+    -- Convert socket to unbuffered handle
+    hdl    <- socketToHandle socket ReadWriteMode
+    hSetBuffering hdl NoBuffering
+    
     netConverse hdl []
     netConverse hdl ["VERSION 1.0"]
     netConverse hdl ["ID asdf12364"]
+    
     -- So close!
     hClose hdl
 
 
-client host port = withSocketsDo $ do
-    addrInfo   <- getAddrInfo (Just hints) (Just host) (Just $ show port)
-    serverAddr <- (headByTrial addrInfo)
-    sock       <- socket (addrFamily serverAddr) Stream defaultProtocol
-    connect sock (addrAddress serverAddr)
-    hdl        <- socketToHandle sock ReadWriteMode
-    hSetBuffering hdl NoBuffering
-    putStrLn "successfully connected"
-    return hdl
-
-
-headByTrial [] = error "No valid address to connect to"
-
-headByTrial (x:xs) = do
-    sock <- socket (addrFamily x) Stream defaultProtocol
-    res <- try $ connect sock  (addrAddress x)
-    case res of
-        Left (SomeException e) ->  headByTrial xs
-        Right _ -> do sClose sock ; return x
