025d0d46c020cf29a7d9a82c0345e98246b440e9
Skrupellos <skruppy@onmars.eu>
2016-03-04 22:49:09 +0100

    Nicer network functions

--- network.hs
+++ network.hs
@@ -2,49 +2,66 @@ import Network.Socket as NS
 import qualified Data.ByteString.Char8 as BS8
 import Control.Exception
 import Control.Monad.Error
+-- import Sm
+import System.IO
+import System.Console.ANSI
 
 hints = NS.defaultHints { NS.addrFlags = [NS.AI_ADDRCONFIG], NS.addrSocketType = NS.Stream }
 
-main = do sock <- client "localhost" 1357 -- get a connected socket
-          msg <- recv sock 100 -- to receive pass the socket and a bytelength
-          putStrLn msg
-          bytesSent <- send sock "VERSION 1.0\n" -- to send pass the socket and a message -> returns bytes sent
-          putStrLn $ show bytesSent
-          msg1 <- recv sock 100
-          putStr msg1
-          --putStrLn ""
-          bytesSent1 <- send sock "ID asdf12364\n"
-          putStrLn $ show bytesSent1
-          msg2 <- recv sock 2048
-          putStr msg2 
-         -- putStrLn ""
-          msg3 <- recv sock 2048
-          putStr msg3
-        --  putStrLn ""
-          msg4 <- recv sock 2048
-          putStr msg4
-       --   putStrLn ""
-          sClose sock
 
-client :: String -> Int -> IO (Socket)
+netListen hdl = do
+    msg <- hGetLine hdl
+    putStr "S: "
+    
+    setSGR [SetColor Foreground Vivid Magenta, SetConsoleIntensity BoldIntensity]
+    putStrLn msg
+    setSGR [Reset]
+    
+    return msg
+
+
+netSpeak hdl line = do
+    putStr "C: "
+    
+    setSGR [SetColor Foreground Vivid Cyan, SetConsoleIntensity BoldIntensity]
+    putStrLn line
+    setSGR [Reset]
+    
+    hPutStrLn hdl line
+
+
+netConverse hdl [] = netListen hdl
+
+netConverse hdl (line:remaining) = do
+    netSpeak hdl line
+    netConverse hdl remaining
+
+
+main = do
+    hdl <- client "localhost" 1357 -- get a connected socket
+    netConverse hdl []
+    netConverse hdl ["VERSION 1.0"]
+    netConverse hdl ["ID asdf12364"]
+    -- So close!
+    hClose hdl
+
+
 client host port = withSocketsDo $ do
-                addrInfo <- getAddrInfo (Just hints) (Just host) (Just $ show port)
-                serverAddr <- (headByTrial addrInfo)
-                sock <- socket (addrFamily serverAddr) Stream defaultProtocol
-                connect sock (addrAddress serverAddr)
-                putStrLn "successfully connected"
-                return sock
-                --msgSender sock
-                --sClose sock
-                
-headByTrial (x:xs) = 
-                if (x:xs) == [] then    do  putStrLn "No valid address to connect to" -- empty list
-                                            error "No valid address to connect to" 
-                            else
-                                    do  sock <- socket (addrFamily x) Stream defaultProtocol
-                                        res <- try $ connect sock  (addrAddress x)
-                                        case res of 
-                                            Left (SomeException e) ->  headByTrial xs
-                                            Right _ -> do   sClose sock
-                                                            return x -- return the first good address
-                                                            
+    addrInfo   <- getAddrInfo (Just hints) (Just host) (Just $ show port)
+    serverAddr <- (headByTrial addrInfo)
+    sock       <- socket (addrFamily serverAddr) Stream defaultProtocol
+    connect sock (addrAddress serverAddr)
+    hdl        <- socketToHandle sock ReadWriteMode
+    hSetBuffering hdl NoBuffering
+    putStrLn "successfully connected"
+    return hdl
+
+
+headByTrial [] = error "No valid address to connect to"
+
+headByTrial (x:xs) = do
+    sock <- socket (addrFamily x) Stream defaultProtocol
+    res <- try $ connect sock  (addrAddress x)
+    case res of
+        Left (SomeException e) ->  headByTrial xs
+        Right _ -> do sClose sock ; return x
