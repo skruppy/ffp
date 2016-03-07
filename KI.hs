79c32d655151de1c0de196a89f83133af2a146c8
Albatrouss <daniel-haemmerle@hotmail.com>
2016-03-07 02:06:45 +0100

    added functions

--- KI.hs
+++ KI.hs
@@ -1,49 +1,75 @@
-import Data.Tree.Game_tree.Negascout
-import Data.Tree.Game_tree.Game_tree
+import Data.Tree.Game_tree.Negascout as NS
+import Data.Tree.Game_tree.Game_tree as GT
 import Data.Array
 
 
 data RNode = 
     RNode { gamefield       :: (Array (Int, Int) (Int, String)),
             playerColour    :: String, -- playerNumber 0 => B; 1 => W
-            playerTurn      :: String --whose turn it is --> B/W
-            } deriving Show 
+            playerTurn      :: String, --whose turn it is --> B/W
+            lastMove        :: Maybe (Int,Int)
+            } deriving (Eq, Show)
+--              Up   UpRight Right DownRight Down DownLeft Left   UpLeft
+directions = [(-1,0),(-1,1), (0,1),(1,1),    (1,0),(1,-1), (0,-1),(-1,-1)]            
             
 is_terminal :: RNode -> Bool
-is_terminal a = (countUp a) && (countUpRight a) && (countRight a) && (countDownRight a) && (countDown a) && (countDownLeft a) && (countLeft a) && (countUpLeft a)
-
-
-countUp         a = countB    (-1)    0       a
-countUpRight    a = countB    (-1)    1       a
-countRight      a = countB    0       1       a
-countDownRight  a = countB    1       1       a
-countDown       a = countB    1       0       a
-countDownLeft   a = countB    1       (-1)    a
-countLeft       a = countB    0       (-1)    a
-countUpLeft     a = countB    (-1)    (-1)    a
-
---countB ::  Int -> Int -> RNode -> Bool
-countB rC cC (RNode field p t) = False
-
-
+is_terminal a = ( [] == ( cchildren a) )
 
 node_value :: RNode -> Int
-node_value (RNode field p t) = if p == t then nv else (-1 * nv)
+node_value (RNode field p t _) = if p == t then nv else (-1 * nv)
     where nv = foldl (\acc (x,_) -> x+acc) 0 $ elems field
 
 
 
-createWeightedArray :: (Array (Int, Int) String) -> (Array (Int, Int) (Int, String))
-createWeightedArray field = listArray (bounds field) list
-    where list = makeElemList $ elems field
+createWeightedArray :: String -> (Array (Int, Int) String) -> (Array (Int, Int) (Int, String))
+createWeightedArray pC field = listArray (bounds field) list
+    where list = makeElemList pC (elems field) 
           
     
-makeElemList :: [String] -> [(Int, String)]
-makeElemList [] = []
-makeElemList ("*":xs) = (0,"*") : makeElemList xs
-makeElemList ("W":xs) = (1,"W") : makeElemList xs
-makeElemList ("B":xs) = ((-1),"B") : makeElemList xs
-makeElemList (x:xs) = (minBound,x) : makeElemList xs
+makeElemList :: String -> [String] -> [(Int, String)]
+makeElemList _ [] = []
+makeElemList pC ("*":xs) = (0,"*") : makeElemList pC xs
+makeElemList pC (x:xs) = if (pC == x) then (1,pC) : makeElemList pC xs else ((-1),notPC) : makeElemList pC xs
+    where notPC = if pC == "W" then "B" else "W"
 
 
---TODO: children :: RNode -> [RNode]
+cchildren :: RNode -> [RNode]
+cchildren (RNode field pC pT lM) = []
+    
+
+getValidMoves :: (Array (Int,Int) (Int,String)) -> String -> [(Int, Int)]
+getValidMoves field pT =  throwOutZeros i $ map (lookInAllDirectionsFrom field pT) i
+    where i = indices field
+          throwOutZeros :: [(Int,Int)] -> [Int] -> [(Int,Int)]
+          throwOutZeros [] _ = []
+          throwOutZeros _ [] = []
+          throwOutZeros (y:ys) (0:xs) = throwOutZeros ys xs
+          throwOutZeros (y:ys) (x:xs) = y: throwOutZeros ys xs
+
+    
+lookInAllDirectionsFrom :: (Array (Int,Int) (Int,String)) -> String -> (Int,Int) -> Int
+lookInAllDirectionsFrom field pT (r,c) = sum $ map (lookInDirectionFrom field pT (r,c)) directions
+    
+
+-- from any "*" in field goes in the direction defined by dirRow and dirCol as long as NOT pT (aka the other colour) is present. returns the number of coins flipped for that field in that direction.
+-- is 0 if in the specified direction a) no stone of colour comes or b) a stone of same colour comes immediatly or c) a stone of same colour doesnt come after other coloured stones 
+lookInDirectionFrom :: (Array (Int,Int) (Int,String)) -> String -> (Int,Int) -> (Int, Int) -> Int
+lookInDirectionFrom field pT (r,c) (dirRow, dirCol) = if ((field ! (r,c)) == ( 0 ,"*")) then countIt (r+dirRow) (c+dirCol) 0 else 0
+    where checkIt nR nC = if ((nR <= ubR) && (nC <= ubC) && (nR >=lbR) && (nC >= lbC)) then True else False 
+          countIt nR' nC' acc = if (checkIt nR' nC')    then 
+            if ((field ! (nR', nC')) == (0,"*"))            then 0      else
+            if ((field ! (nR', nC')) == (1,pT)&& acc >0)    then acc    else
+            countIt (nR'+dirRow) (nC'+dirCol) (acc +1)  else 0 
+          ((lbR,lbC),(ubR, ubC)) = bounds field
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
