bab8c05ca94945a8acd65734cb7af759762b03cb
Albatrouss <daniel-haemmerle@hotmail.com>
2016-03-07 02:43:32 +0100

    finished functions needed for game_tree

--- KI.hs
+++ KI.hs
@@ -9,11 +9,12 @@ data RNode =
             playerTurn      :: String, --whose turn it is --> B/W
             lastMove        :: Maybe (Int,Int)
             } deriving (Eq, Show)
+            
 --              Up   UpRight Right DownRight Down DownLeft Left   UpLeft
 directions = [(-1,0),(-1,1), (0,1),(1,1),    (1,0),(1,-1), (0,-1),(-1,-1)]            
             
 is_terminal :: RNode -> Bool
-is_terminal a = ( [] == ( cchildren a) )
+is_terminal a = ([] == (cchildren a))
 
 node_value :: RNode -> Int
 node_value (RNode field p t _) = if p == t then nv else (-1 * nv)
@@ -34,7 +35,19 @@ makeElemList pC (x:xs) = if (pC == x) then (1,pC) : makeElemList pC xs else ((-1
 
 
 cchildren :: RNode -> [RNode]
-cchildren (RNode field pC pT lM) = []
+cchildren (RNode field pC pT lM) = makeChildrenFromMoves (getValidMoves field pT) field pC pT
+
+makeChildrenFromMoves :: [(Int,Int)] -> (Array (Int,Int) (Int,String)) -> String -> String -> [RNode]
+makeChildrenFromMoves [] _ _ _ = []
+makeChildrenFromMoves (x:xs) field pC pT = (RNode (makeNewField x pT pC field) pC notPT (Just x)) : makeChildrenFromMoves xs field pC pT
+    where notPT = if pT == "W" then "B" else "W"
+          
+makeNewField :: (Int,Int) -> String -> String -> (Array (Int,Int) (Int,String)) -> (Array (Int,Int) (Int,String))
+makeNewField i pT pC field = field // changes
+    where changes = splitToChanges directions i $ map (lookInDirectionFrom field pT i) directions
+          splitToChanges _ _ [] = []
+          splitToChanges (y:ys)(r,c)(0:xs) = splitToChanges ys (r,c) xs
+          splitToChanges ((dr,dc):ys)(r,c)(x:xs) = ((r+dr*x,c+dc*x), (head (makeElemList pC [pT]))) : splitToChanges ((dr,dc):ys)(r,c)((x-1):xs)
     
 
 getValidMoves :: (Array (Int,Int) (Int,String)) -> String -> [(Int, Int)]
@@ -44,7 +57,7 @@ getValidMoves field pT =  throwOutZeros i $ map (lookInAllDirectionsFrom field p
           throwOutZeros [] _ = []
           throwOutZeros _ [] = []
           throwOutZeros (y:ys) (0:xs) = throwOutZeros ys xs
-          throwOutZeros (y:ys) (x:xs) = y: throwOutZeros ys xs
+          throwOutZeros (y:ys) (x:xs) = y : throwOutZeros ys xs
 
     
 lookInAllDirectionsFrom :: (Array (Int,Int) (Int,String)) -> String -> (Int,Int) -> Int
@@ -55,21 +68,11 @@ lookInAllDirectionsFrom field pT (r,c) = sum $ map (lookInDirectionFrom field pT
 -- is 0 if in the specified direction a) no stone of colour comes or b) a stone of same colour comes immediatly or c) a stone of same colour doesnt come after other coloured stones 
 lookInDirectionFrom :: (Array (Int,Int) (Int,String)) -> String -> (Int,Int) -> (Int, Int) -> Int
 lookInDirectionFrom field pT (r,c) (dirRow, dirCol) = if ((field ! (r,c)) == ( 0 ,"*")) then countIt (r+dirRow) (c+dirCol) 0 else 0
-    where checkIt nR nC = if ((nR <= ubR) && (nC <= ubC) && (nR >=lbR) && (nC >= lbC)) then True else False 
-          countIt nR' nC' acc = if (checkIt nR' nC')    then 
-            if ((field ! (nR', nC')) == (0,"*"))            then 0      else
-            if ((field ! (nR', nC')) == (1,pT)&& acc >0)    then acc    else
-            countIt (nR'+dirRow) (nC'+dirCol) (acc +1)  else 0 
+    where checkNext nR nC = if ((nR <= ubR) && (nC <= ubC) && (nR >=lbR) && (nC >= lbC)) then True else False 
+          countIt nR' nC' acc = if (checkNext nR' nC')            then 
+                if ((field ! (nR', nC')) == (0,"*"))            then 0      else
+                if ((field ! (nR', nC')) == (1,pT)&& acc >0)    then acc    else
+                countIt (nR'+dirRow) (nC'+dirCol) (acc +1)                  else 0 
+                
           ((lbR,lbC),(ubR, ubC)) = bounds field
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
+                  
