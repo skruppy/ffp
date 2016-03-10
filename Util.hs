2dd4e31545822200c82e8b001b93b59d1b406094
Skrupellos <skruppy@onmars.eu>
2016-03-10 19:48:44 +0100

    Added LICENSE

--- Util.hs
+++ Util.hs
@@ -1,3 +1,9 @@
+-- "THE BEER-WARE LICENSE" (Revision 42):
+-- Albatrouss and <skruppy@onmars.eu> wrote this software. As long as you retain
+-- this notice you can do whatever you want with this stuff. If we meet some
+-- day, and you think this stuff is worth it, you can buy me a beer in return.
+--                                                     -- Albatrouss and Skruppy
+
 module Util where
 
 mergeMaybe (Just a)    _        = Just a
--- sysprak.hs
+++ sysprak.hs
@@ -1,3 +1,9 @@
+-- "THE BEER-WARE LICENSE" (Revision 42):
+-- Albatrouss and <skruppy@onmars.eu> wrote this software. As long as you retain
+-- this notice you can do whatever you want with this stuff. If we meet some
+-- day, and you think this stuff is worth it, you can buy me a beer in return.
+--                                                     -- Albatrouss and Skruppy
+
 import Sm as S
 import Net
 import KI as KI
