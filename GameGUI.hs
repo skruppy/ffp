74e67db74589d12258ba5f12056d3abecec183dc
Albatrouss <daniel-haemmerle@hotmail.com>
2016-03-12 00:25:55 +0100

    added players to GUI and Gametype and name to title

--- GameGUI.hs
+++ GameGUI.hs
@@ -7,6 +7,7 @@
 module GameGUI where
 
 import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Display.Label
 import KI
 import Sm
 import Data.Array
@@ -17,22 +18,41 @@ import Control.Concurrent.MVar
 createGameGUI :: (MVar (Array (Int, Int) String))-> (MVar GameData) -> IO ()
 createGameGUI mVField  mVGameData = do 
     initGUI
-    window <- windowNew
-    set window [windowTitle := "GUI", containerBorderWidth := 5,
-                windowDefaultWidth := 500, windowDefaultHeight := 500]
-                
     field <- takeMVar mVField
     gameData <- takeMVar mVGameData
-    let ((_,_),(_,size)) = bounds field
-    table <- tableNew (size +1) (size +1) True 
+    let getGameTypeAndName (GameData _ _ gt gn _) = gt ++ " : " ++ gn
+    let title = getGameTypeAndName gameData
+    window <- windowNew
+    set window [windowTitle := title, containerBorderWidth := 5,
+                windowDefaultWidth := 500, windowDefaultHeight := 500]
     
-    buttons <- createButtons field table
-    addLabels table size
-    containerAdd window table
+    
+    let ((_,_),(_,size)) = bounds field
+    layoutTable <- tableNew 2 1 False
+    -- game field 
+    gameTable <- tableNew (size +1) (size +1) True 
+    tableAttachDefaults layoutTable gameTable 0 1 0 1
+    buttons <- createButtons field gameTable
+    addLabels gameTable size
+    -- player field
+    playerTable <- tableNew 1 2 True
+    let getNameFromPlayer (PlayerItem s _ itsMe) = if itsMe then ("  <b>" ++ s  ++ "</b>") else s
+    let getPlayername (GameData _ _ _ _ players) n = getNameFromPlayer (players ! n)
+    let playername0 = (getPlayername gameData 0) ++ " ⛂"
+    let playername1 = (getPlayername gameData 1) ++ " ⛀"
+    labelP0 <- labelNew (Just playername0 )
+    labelSetMarkup labelP0 playername0
+    tableAttachDefaults playerTable labelP0 0 1 0 1
+    labelP1 <- labelNew (Just playername1)
+    labelSetMarkup labelP1 playername1
+    tableAttachDefaults playerTable labelP1 0 1 1 2
+    tableAttachDefaults layoutTable playerTable 1 2 0 1
+    
+    containerAdd window layoutTable
     
     onDestroy window mainQuit
     widgetShowAll window
-    --windowPresent window
+    
     timeoutAdd ((updateField mVField buttons)>> yield >> return True) 100
     
     mainGUI
@@ -90,10 +110,3 @@ createLabels table coord labelStrings = do
     let trippel = zip labels coord
     mapM_ (\(l,(c,r)) -> tableAttachDefaults table l (r-1) r (c-1) c) trippel
     return labels
-    
-    
-    
-    
-    
-    
-    
